eb202760920b80dbd8547282ac3d2060373f5f4f
ductility <jys9704@gmail.com>
2020-07-26 01:25:18 +0900

    Add PWA

---
+++ server/public/precache-manifest.2792d9d530b06b5d3259e7d68a89cc7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157c50ae5031cb2ea740",
+    "url": "/css/app.b9772450.css"
+  },
+  {
+    "revision": "493929d3addd702ed634",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "d2ef5352a399f0fb6d452c6ac8c6c96a",
+    "url": "/img/download.png"
+  },
+  {
+    "revision": "a38c28cff191d112be4afbb7b9ace3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ef1512c138884e4d70be93b8323bed",
+    "url": "/install.js"
+  },
+  {
+    "revision": "157c50ae5031cb2ea740",
+    "url": "/js/app.19ffb236.js"
+  },
+  {
+    "revision": "493929d3addd702ed634",
+    "url": "/js/chunk-vendors.afb3cf70.js"
+  },
+  {
+    "revision": "1e38c21c33c2efea4c30cba6b0b4d911",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3e9849905ef35925d023997cd6cc1afc",
+    "url": "/sw.js"
+  }
+]);
